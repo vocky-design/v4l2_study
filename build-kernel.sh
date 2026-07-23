8f10f0615ee7ab97c6abf4a5b78978f832d297ba
vocky <sword1458364124@foxmail.com>
2026-07-23 22:35:16 +0800

    Add build script for Linux kernel version 6.10

---
+++ build-kernel.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+VER=6.10
+KERNEL_DIR=linux-${VER}
+
+if [ ! -d ${KERNEL_DIR} ];then
+    wget https://mirrors.hust.edu.cn/kernel.org/linux/kernel/v6.x/linux-${VER}.tar.xz
+    tar -xf linux-${VER}.tar.xz
+fi
+
+cd ${KERNEL_DIR}
+make x86_64_defconfig
+# 开启模块支持、调试信息
+
+make -j$(nproc)
+cd ..
+echo "内核编译完成: ${KERNEL_DIR}/arch/x86/boot/bzImage"
